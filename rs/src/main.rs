b0be204e6008a2aa12d476b89f9f4fb41024e29f
Cardoso <gabriel9225@gmail.com>
2022-06-21 00:12:43 -0300

    Retorna vetor ao invés de usar variável de entrada e saida

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -36,7 +36,7 @@ impl Graph {
         self.edges.push(e)
     }
     
-    fn find_by_id(&mut self, id: usize) -> Rc<Vertex> {
+    fn find_by_id(&self, id: usize) -> Rc<Vertex> {
         self.verts.iter().find(|v| v.id == id).cloned().unwrap()
     }
 
@@ -45,7 +45,7 @@ impl Graph {
         let mut buf_reader = BufReader::new(file);
         let mut contents = String::new();
         buf_reader.read_to_string(&mut contents).expect("Não foi possível transformar arquivo em String");
-        let split = contents.split("\n");
+        let split = contents.split('\n');
         let mut vertices_finished = false;
         let mut is_directed = true;
 
@@ -64,34 +64,36 @@ impl Graph {
             }
 
             if !vertices_finished {
-                let mut vec: Vec<&str> = line.splitn(2, " ").collect();
-                let mut v = Vertex{id: vec[0].parse::<usize>().unwrap(), name: vec[1].to_string()};
+                let vec: Vec<&str> = line.splitn(2, ' ').collect();
+                let v = Vertex{id: vec[0].parse::<usize>().unwrap(), name: vec[1].to_string()};
                 self.add_vertex(Rc::new(v));
             } else {
-                let vec: Vec<&str> = line.splitn(3, " ").collect();
+                let vec: Vec<&str> = line.splitn(3, ' ').collect();
                 let v = self.find_by_id(vec[0].parse::<usize>().unwrap());
                 let u = self.find_by_id(vec[1].parse::<usize>().unwrap());
                 let w = vec[2].parse::<f32>().unwrap();
                 self.add_edge(Rc::new(Edge{v: Rc::clone(&v), u: Rc::clone(&u), w}));
-                if !is_directed { self.add_edge(Rc::new(Edge{v: Rc::clone(&u), u: Rc::clone(&v), w: w })) }
+                if !is_directed { self.add_edge(Rc::new(Edge{v: Rc::clone(&u), u: Rc::clone(&v), w })) }
             }
         }
     }
 
-    pub fn get_edges(&self, v: Rc<Vertex>, result: &mut Vec<Rc<Edge>>) {   
+    pub fn get_edges(&self, v: Rc<Vertex>) ->  Vec<Rc<Edge>> {
+        let mut edges: Vec<Rc<Edge>> = vec![];
         for edge in &self.edges {
             if edge.v == v {
-                result.push(Rc::clone(&edge));
+                edges.push(Rc::clone(edge));
             }
         }
+        edges
     }
 
-    pub fn get_neighbours(&self, v: Rc<Vertex>, result: &mut Vec<Rc<Vertex>>) {
-        let mut collector = Vec::new();
-        self.get_edges(v, &mut collector);
-        for edge in collector {
+    pub fn get_neighbours(&self, v: Rc<Vertex>) -> Vec<Rc<Vertex>> {
+        let mut result = Vec::new();
+        for edge in self.get_edges(v) {
             result.push(Rc::clone(&edge.u));
         }
+        result
     }
 }
 
@@ -100,9 +102,10 @@ fn main() {
     let mut graph = Graph::new();
     graph.load(path);
     println!("Arquivo carregado com sucesso!");
-    let mut neighbours: Vec<Edge> = Vec::new();
-    //test.get_neighbours(0,&mut neighbours);
-    //if let Some(edge) = neighbours.pop() {
-     //   println!("Achou vizinho {}", edge.u_pos);
-    //}
+    let some_v = graph.find_by_id(1);
+    let neighbours = graph.get_neighbours(some_v.clone());
+    for v in neighbours {
+        println!("Vertex: {}, Neighbors{}", some_v.name, v.name)
+    }
 }
+
